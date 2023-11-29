815bd3b96ceb10473f30668034763ae0a1564db9
Megan Perry <144282117+clickclickbang57@users.noreply.github.com>
2023-11-29 15:14:56 -0700

    made the page work

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,158 @@
-console.log('Script file loaded.');
-
-// Define an array of chocolate images, each image appearing twice
-var chocoImages = ['black-choco.png', 'blue-choco.png', 'pink-choco.png', 'green-choco.png', 'red-choco.png', 'yellow-choco.png'];
-chocoImages = chocoImages.concat(chocoImages);
-
-// Shuffle the array
-chocoImages.sort(function() {
-    return 0.5 - Math.random();
-});
-
-// Get all card elements
-var cards = document.querySelectorAll('.card');
-
-// Assign images to cards
-for (var i = 0; i < cards.length; i++) {
-    var chocoImg = cards[i].querySelector('.choco-img');
-    chocoImg.src = 'Images/' + chocoImages[i];
-}
-
-// Game logic
-var firstCard, secondCard;
-document.body.addEventListener('click', function(event) {
-    var clickedCard = event.target.parentElement;
-    if (clickedCard.classList.contains('card')) {
-        if (!firstCard) {
-            firstCard = clickedCard;
-            revealCard(firstCard);
-        } else if (!secondCard) {
-            secondCard = clickedCard;
-            revealCard(secondCard);
-
-            if (firstCard.querySelector('.choco-img').src === secondCard.querySelector('.choco-img').src) {
-                firstCard = secondCard = null;
-            } else {
-                setTimeout(function() {
-                    hideCard(firstCard);
-                    hideCard(secondCard);
-                    firstCard = secondCard = null;
-                }, 1000);
-            }
-        }
+const CARDS = [
+    {
+      id: 1,
+      name: 'black-choco',
+      img: 'images/black-choco.png'
+    },
+    {
+      id: 2,
+      name: 'blue-choco',
+      img: 'images/blue-choco.png'
+    },
+    {
+      id: 3,
+      name: 'green-choco',
+      img: 'images/green-choco.png'
+    },
+    {
+      id: 4,
+      name: 'pink-choco',
+      img:
+        'images/pink-choco.png'
+    },
+    {
+      id: 5,
+      name: 'red-choco',
+      img: 'images/red-choco.png'
+    },
+    {
+      id: 6,
+      name: 'white-choco',
+      img: 'images/white-choco.png'
+    },
+    {
+      id: 7,
+      name: 'yellow-choco',
+      img:
+        'images/yellow-choco.png'
     }
-});
-
-function revealCard(card) {
-    card.querySelector('.card-back').style.display = 'none';
-    card.querySelector('.choco-img').style.display = 'block';
-}
-
-function hideCard(card) {
-    card.querySelector('.card-back').style.display = 'block';
-    card.querySelector('.choco-img').style.display = 'none';
-}
+  ];
+  const cardContainer = document.querySelector('.card-container');
+  const available = document.querySelector('#available');
+  const modalTitle = document.querySelector('#modal-title');
+  const modal = document.querySelector('#modal');
+  let currentCards = [...CARDS, ...CARDS];
+  let isPaused = false;
+  let counter = CARDS.length + 10;
+  let isLose = false;
+  
+  // Fisher--Yates Algorithm -- https://bost.ocks.org/mike/shuffle/
+  function shuffle(array) {
+    let counter = array.length,
+      temp,
+      index;
+    while (counter > 0) {
+      index = Math.floor(Math.random() * counter);
+      counter--;
+      temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+    }
+    return array;
+  }
+  
+  function win() {
+    isPaused = true;
+    modalTitle.innerHTML = 'You win! 🙌🥳';
+    modal.classList.add('modal--open');
+  }
+  
+  function lose() {
+    isLose = true;
+    modalTitle.innerHTML = 'You lose 😢😩';
+    modal.classList.add('modal--open');
+  }
+  
+  function handleClick(e) {
+    const { target } = e;
+    if (
+      !isPaused &&
+      !isLose &&
+      !target.classList.contains('card--guessed') &&
+      !target.classList.contains('card--picked')
+    ) {
+      isPaused = true;
+      const picked = cardContainer.querySelector('.card--picked');
+      if (picked) {
+        if (picked.dataset.id === target.dataset.id) {
+          target.classList.remove('card--picked');
+          picked.classList.remove('card--picked');
+          target.classList.add('card--guessed');
+          picked.classList.add('card--guessed');
+          isPaused = false;
+        } else {
+          target.classList.add('card--picked');
+          setTimeout(() => {
+            target.classList.remove('card--picked');
+            picked.classList.remove('card--picked');
+            isPaused = false;
+          }, 1500);
+        }
+        console.log('counter', counter);
+        counter -= 1;
+        available.innerHTML = counter;
+        if (counter === 0) {
+          lose();
+        }
+      } else {
+        target.classList.add('card--picked');
+        isPaused = false;
+      }
+  
+      // Validate is already win
+      const isWin = cardContainer.querySelectorAll('card--guessed').length === currentCards.length;
+      if (isWin) {
+        win();
+      }
+    }
+  }
+  
+  function drawCards() {
+    cardContainer.innerHTML = '';
+    available.innerHTML = counter;
+  
+    shuffle(currentCards).forEach((el) => {
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.setAttribute('data-id', el.id);
+      card.innerHTML = `
+          <div class="card__front">
+            <img
+              class="front__img"
+              src="${el.img}"
+              alt="${el.name}"
+            />
+          </div>
+          <div class="card__back">
+            <img
+              class="back__img"
+              src="images/card back.png"
+              alt="Thought"
+            />
+          </div>
+        `;
+      card.addEventListener('click', handleClick);
+      cardContainer.appendChild(card);
+    });
+  }
+  
+  document.querySelector('#play-again').addEventListener('click', function () {
+    modal.classList.remove('modal--open');
+    isPaused = false;
+    isLose = false;
+    counter = CARDS.length + 10;
+    drawCards();
+  });
+  
+  drawCards();
+  
